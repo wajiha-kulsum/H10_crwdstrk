73ba2c62dd63f3e7266ea0f8084aeb841088e6be
0xsoydev <0x00zer0day@gmail.com>
2024-12-28 12:14:57 +0530

    Added Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/myComponents/Footer";
+import Hero from "@/myComponents/Hero";
 import Navbar from "@/myComponents/Navbar";
 
 export default function Home() {
@@ -6,6 +8,12 @@ export default function Home() {
       <div className="flex flex-col h-screen px-8 gap-4">
         <div className="pt-4">
           <Navbar />
+        </div>
+        <div className="flex-1">
+          <Hero />
+        </div>
+        <div>
+          <Footer />
         </div>
       </div>
     </>
